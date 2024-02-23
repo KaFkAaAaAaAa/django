73450dd8b8299c1c5bbfa41221eec3fb2b7612d8
C04H05 <c04h04@teb.edu.pl>
2024-02-23 13:11:28 +0100

    PIWOPIWOPIWO

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog.models import Post
+
 # Create your views here.
+def post_list(request):
+    #pobiera wszystkie posty do zmiennej
+    posts = Post.objects.all()
+    return HttpResponse(posts)
